8f7be9f2f57294384cb0607d9cfd1605b6a4c93d
S11D0R <dimabezdenezhnyh@mail.ru>
2017-05-23 14:43:37 +0300

    window mode done. Need working Talk Button

--- SayHey_client/src/App/Application.java
+++ SayHey_client/src/App/Application.java
@@ -2,7 +2,8 @@ package App;
 
 
 public class Application {
+    public static MainFrame mainFrame;
     public static void main(String[] args) {
-        MainFrame mainFrame = new MainFrame();
+        mainFrame = new MainFrame();
     }
 }
